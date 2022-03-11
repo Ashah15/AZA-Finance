4653faec210cb5cb68a90b0f6c0afa9cee22db91
Ashah15 <ashaabdillahi53@gmail.com>
2022-03-11 16:43:33 +0300

    Add controller transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,31 @@
 class TransactionsController < ApplicationController
+  before_action :logged_in_user
+
+	def create
+      @transaction = Transaction.new(transactions_params)
+      if @transaction.save
+       render json: @transaction.to_json
+      else
+       render json: @transaction.error, status: :not_accessible
+      end	
+	end
+
+	def index
+	  @transaction = Transaction.all		
+	end
+
+	def show
+    @transaction = Transaction.find(params[:id])
+	end
+
+	private
+
+	def transactions_params
+	  params.require(:transaction).permit(:input_amount, :output_amount, :output_currency, :input_currency, :customer)	
+	end
+
+	def transaction_owner
+	  @transaction = Transaction.find(params[:id])
+	  redirect_to transactions_path unless current_user == current_customer
+	end
 end
